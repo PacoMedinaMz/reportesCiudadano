f00ea43f43d1f23cfea9de60ac3aee0f48ce0526
blank-mpr <jor_manuel186@hotmail.com>
2020-11-30 20:37:52 -0600

    Reparé todo lo que no jalaba de mi parte (angular bootstrap, mdb, etc)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '../app/home/home.component';
+import { ReportesComponent} from '../app/reportes/reportes.component';
+import { ConsultasComponent} from '../app/consultas/consultas.component';
+import { FooterComponent} from '../app/footer/footer.component';
+import { RAnonimoComponent} from '../app/r-anonimo/r-anonimo.component';
+import { RUrgenteComponent} from '../app/r-urgente/r-urgente.component';
+import { RNormalComponent} from '../app/r-normal/r-normal.component';
+import { PgUnoComponent } from '../app/pg-uno/pg-uno.component';
+import { PgDosComponent } from '../app/pg-dos/pg-dos.component';
+import { PgTresComponent } from '../app/pg-tres/pg-tres.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component:HomeComponent},
+  {path: 'reportes', component:ReportesComponent},
+  {path: 'consultas', component:ConsultasComponent},
+  {path: 'footer', component:FooterComponent},
+  {path: 'RAnonimo', component:RAnonimoComponent},
+  {path: 'RUrgente', component:RUrgenteComponent},
+  {path: 'RNormal', component:RNormalComponent},
+  {path: 'pUno', component:PgUnoComponent},
+  {path: 'pDos', component:PgDosComponent},
+  {path: 'pTres', component:PgTresComponent},
+  {path: '**', pathMatch:'full', redirectTo: 'home'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,54 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ReportesComponent } from './reportes/reportes.component';
+import { ConsultasComponent } from './consultas/consultas.component';
+import { FooterComponent } from './footer/footer.component';
+import { RUrgenteComponent } from './r-urgente/r-urgente.component';
+import { RAnonimoComponent } from './r-anonimo/r-anonimo.component';
+import { RNormalComponent } from './r-normal/r-normal.component';
+import { ToastModule, IconsModule, SelectModule } from 'ng-uikit-pro-standard';
+import { ButtonsModule, WavesModule, CardsModule, } from 'angular-bootstrap-md';
+import { PgUnoComponent } from './pg-uno/pg-uno.component';
+import { PgDosComponent } from './pg-dos/pg-dos.component';
+import { PgTresComponent } from './pg-tres/pg-tres.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ReportesComponent,
+    ConsultasComponent,
+    FooterComponent,
+    RUrgenteComponent,
+    RAnonimoComponent,
+    RNormalComponent,
+    PgUnoComponent,
+    PgDosComponent,
+    PgTresComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot(),
+    FormsModule,
+    AppRoutingModule,
+    ToastModule.forRoot(), 
+    IconsModule,
+    ButtonsModule, 
+    WavesModule, 
+    CardsModule,
+    SelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
